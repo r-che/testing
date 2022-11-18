b5610b53468e74414bbb78852d04b88cc913a6af
RomanChebotarev <RomanCh@artel.im>
2022-11-18 18:26:43 +0300

    debug - rename PrintFlag to PrintFlags, add PrintFlags.{Is,Not}, add PrintValPerLine, simplify code

--- debug/print.go
+++ debug/print.go
@@ -2,18 +2,28 @@ package debug
 
 import "fmt"
 
-type PrintFlag byte
+type PrintFlags byte
+
+func (pf PrintFlags) Is(flagsSet PrintFlags) bool {
+	return pf & flagsSet != 0
+}
+
+func (pf PrintFlags) Not(flagsSet PrintFlags) bool {
+	return pf & flagsSet == 0
+}
+
 const PrintNoFlags	=	0
 const (
-	PrintType		=	PrintFlag(1) << iota
+	PrintType		=	PrintFlags(1) << iota
 	PrintCommaSep
 	PrintNoSharp
 	PrintGoSyntax
 	PrintLenCap
 	PrintValType
+	PrintValPerLine
 )
 
-func PrintSlice[T any](slice []T, flagsVariadic ...PrintFlag) {
+func PrintSlice[T any](slice []T, flagsVariadic ...PrintFlags) {
 	// Open/closed braces
 	obr, cbr := "[", "]"
 
@@ -21,7 +31,7 @@ func PrintSlice[T any](slice []T, flagsVariadic ...PrintFlag) {
 	flags := mergeFlags(flagsVariadic)
 
 	// Is printing of slice type required?
-	if flags & PrintType != 0 {
+	if flags.Is(PrintType) {
 		// Print slice type
 		fmt.Printf("%T", slice)
 		// Replace open/closed braces to make Go-like output
@@ -29,45 +39,95 @@ func PrintSlice[T any](slice []T, flagsVariadic ...PrintFlag) {
 	}
 
 	// Is printing of length and capacity required?
-	if flags & PrintLenCap != 0 {
+	if flags.Is(PrintLenCap) {
 		fmt.Printf("(%d:%d)", len(slice), cap(slice))
 	}
 
 	// Output format
-	outFmt := ""
-	if flags & PrintNoSharp == 0 {
-		outFmt += "#"
-	}
-	outFmt += "%d%s:%"
-	if flags & PrintGoSyntax != 0 {
-		outFmt += "#"
-	}
-	outFmt += "v"
+	outFmt := itemFmt(flags)
 
 	// Print open brace
 	fmt.Print(obr)
+
+	// Is only one value per line to be printed?
+	if flags.Is(PrintValPerLine) {
+		// Print new line before the first item
+		fmt.Println()
+	}
+
+	// Output items
+	printSliceItems(outFmt, slice, flags)
+
+	// Print closed brace
+	fmt.Println(cbr)
+}
+
+func itemFmt(flags PrintFlags) string {
+	// Output format
+	outFmt := ""
+
+	// Is only one value per line to be printed?
+	if flags.Is(PrintValPerLine) {
+		// Need to add indentation (2 spaces)
+		outFmt += "  "
+	}
+
+	// Is printing sharp has not disabled?
+	if flags.Not(PrintNoSharp) {
+		// Append sharp sign
+		outFmt += "#"
+	}
+
+	// Appnd position, value type specificator and colon before the value
+	outFmt += "%d%s:"
+
+	// Is Go-syntax required in output?
+	if flags.Is(PrintGoSyntax) {
+		// Append alternative value output format
+		outFmt += "%#v"
+	} else {
+		// Append default value output format
+		outFmt += "%v"
+	}
+
+	return outFmt
+}
+
+func printSliceItems[T any](outFmt string, slice []T, flags PrintFlags) {
+	// Items divider
+	var iDiv string
+	if flags.Is(PrintValPerLine) {
+		// Use new line as items separator
+		iDiv = "\n"
+
+		// Also need to print new line at end of the output
+		defer fmt.Println()
+	} else {
+		// Use space as items separator
+		iDiv = " "
+	}
+
 	for i, v := range slice {
 		// Type of value string
 		var valType string
 		// Is it required?
-		if flags & PrintValType != 0 {
+		if flags.Is(PrintValType) {
 			// Set value
 			valType = fmt.Sprintf("(%T)", v)
 		}
 
 		fmt.Printf(outFmt, i, valType, v)
+
 		if i != len(slice) - 1 {
-			if flags & PrintCommaSep != 0 {
+			if flags.Is(PrintCommaSep) {
 				fmt.Print(",")
 			}
-			fmt.Print(" ")
+			fmt.Print(iDiv)
 		}
 	}
-	// Print closed brace
-	fmt.Println(cbr)
 }
 
-func mergeFlags(flagsVariadic []PrintFlag) PrintFlag {
+func mergeFlags(flagsVariadic []PrintFlags) PrintFlags {
 	switch len(flagsVariadic) {
 	// No flags
 	case 0:
@@ -81,7 +141,7 @@ func mergeFlags(flagsVariadic []PrintFlag) PrintFlag {
 
 	// Merge flags from all items of flagsVariadic
 	default:
-		var flags PrintFlag
+		var flags PrintFlags
 		for _, flag := range flagsVariadic {
 			flags |= flag
 		}
